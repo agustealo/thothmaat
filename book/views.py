323662902319bca6a0a99d673c1350a65692327c
Agustealo <agustealo@gmail.com>
2017-03-22 20:17:46 -0400

    Modified templates to display dynamic html  content

--- book/views.py
+++ book/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Leaf
 
-# Create your views here.
 def book_list(request):
-    return render(request, 'book/book_list.html', {})
+    leafs = Leaf.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'book/book_list.html', {'leafs': leafs})
