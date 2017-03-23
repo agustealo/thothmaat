4c46f1fd97bc6a5618b48465297dfe4a22105f12
Agustealo <agustealo@gmail.com>
2017-03-23 01:25:34 -0400

    Edit template to include extendable partial include

--- book/urls.py
+++ book/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-app_name = 'book'
 urlpatterns = [
     url(r'^$', views.book_list, name='book_list'),
+    url(r'^book/(?P<pk>\d+)/$', views.book_detail, name='book_detail'),
 ]
